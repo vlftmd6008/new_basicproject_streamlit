5b0b32f644afa3c73eb48718685281cd2d2b1756
vlftmd6008 <cohok6008@gmail.com>
2025-06-22 19:44:40 +0900

    git add . git commit -m "new" git push

--- new_bp_sl.py
+++ new_bp_sl.py
@@ -116,6 +116,8 @@ st.markdown("""🔷 **4.** 계산된 가중치를 활용해, 각 법정동의 �
 topN = pd.read_csv("topN.csv", encoding='utf-8-sig')
 N = st.number_input(f"{name}님이 원하시는 상위 법정동 개수를 선택해주세요. (예: 5, 10)", value=10)
 top = topN.head(N)
+
+
 st.write(f"다음은 상위 {N}개의 법정동 리스트입니다.")
 st.dataframe(top)
 filtered_real_estate = pd.merge(df_final, top, how='inner', on=['CGG_NM', 'STDG_NM'])
